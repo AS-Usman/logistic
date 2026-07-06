17445c4f9cce1b2a4639c1a29e73692970c996f2
AS-Usman <Achakatlashaikusman172@gmail.com>
2026-07-06 18:58:28 +0300

    Delete Phase-1/Day-6 directory

--- Phase-1/Day-6/filterqn.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-df = pd.read_csv("students.csv")
-s1=df[(df["Marks"] > 80) & (df["Backlogs"] == 0)]
-print("Students with Marks above 80 and Zero Backlogs:",s1)
-s2=df[(df["Department"] == "CSE") & (df["Year"] == "3rd Year")]
-print("Students from CSE department in 3rd Year",s2)
-average = df["CGPA"].mean()
-print("Students whose CGPA is above class average",df[df["CGPA"] > average])
